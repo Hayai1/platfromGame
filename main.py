118c17124de55562561a17eef8c4b6cd66619ba8
Hayai1 <dylanpjhough@gmail.com>
2020-09-26 17:54:02 +0100

    implemented: -sprite jumping -in game cursor

--- main.py
+++ main.py
@@ -8,68 +8,91 @@ screen = pygame.display.set_mode(size, 0, 32)
 pygame.display.set_caption('test')
 
 #################################
-spriteRight = pygame.image.load('C://Users//Dylan//PycharmProjects//pypro//sprite folder//1.png')
+spriteRightImg = pygame.image.load('C://Users//Dylan//PycharmProjects//pypro//sprite folder//1.png')
+cursorImg = pygame.image.load('C://Users//Dylan//PycharmProjects//pypro//sprite folder//Cursor.png')
 
 
 ##############################
 class player(object):
     def __init__(self, x, y):
         self.count = 0
-        self.counterpower = 0
+        self.counterPower = 0
         self.x = x
         self.y = y
         self.game1 = True
-        self.walkcount = 0
+        self.walkCount = 0
         self.Right = False
         self.Left = False
         self.jump = False
-        self.jumpcount = 10
+        self.jumpCount = 10.0
         self.count = 0
-        self.startgame = True
+        self.startGame = True
+        self.startPos = 0
 
-    def game(self, screen):
-        if self.walkcount >= 1:
-            self.counterpower = 0
+    def draw(self, screen, spirter):
+        if self.walkCount >= 1.0:
+            self.counterPower = 0.0
         if self.Right:
-            screen.blit(spriteRight, (self.x, self.y))
+            screen.blit(spirter, (self.x, self.y))
         elif self.Left:
-            screen.blit(spriteRight, (self.x, self.y))
+            screen.blit(spirter, (self.x, self.y))
         else:
-            screen.blit(spriteRight, (self.x, self.y))
-        self.walkcount += 1
+            screen.blit(spirter, (self.x, self.y))
+        self.walkCount += 1
 
     pygame.display.update()
 
 
 man = player(90, 90)
+cursor = player(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
 run = True
+pygame.mouse.set_visible(False)
 while run:
-    if man.startgame:
+    cursor.x = pygame.mouse.get_pos()[0]
+    cursor.y = pygame.mouse.get_pos()[1]
+    if man.startGame:
         if man.y < 740:
             man.y += 10
         else:
-            man.startgame = False
+            man.startGame = False
 
     for event in pygame.event.get():
         if event.type == QUIT:
-            loop = False
+            run = False
+
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_LEFT]:
         man.x -= 5
         man.Left = True
-    elif keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT]:
         man.x += 5
         man.Right = True
-    elif keys[pygame.K_SPACE]:
+    if keys[pygame.K_SPACE]:
         man.jump = True
-
     else:
-        man.walkcount = 0
+        man.walkCount = 0
 
-    man.game(screen)
+    if man.jump:
+        if man.jumpCount == 10:
+            man.startPos = man.y
+        neg = 0
+        # moving up
+        if man.jumpCount > 0:
+            neg = 1
+        # moving down
+        if man.jumpCount < 0:
+            neg = -1
+        man.y -= ((man.jumpCount ** 2) * 0.5) * neg
+        man.jumpCount -= 1
+        if man.y >= man.startPos:
+            man.jumpCount = 10
+            man.jump = False
+            man.y = man.startPos
 
+    man.draw(screen, spriteRightImg)
+    cursor.draw(screen, cursorImg)
     pygame.display.flip()
     screen.fill((0, 0, 0))
-    clock.tick(30)
+    clock.tick(240)
 # test
